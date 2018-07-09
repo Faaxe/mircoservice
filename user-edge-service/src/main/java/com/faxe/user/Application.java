8e46307d090a2fd0f1809ef483d58a8685c31faa
ZhouXiang <15951653519@163.com>
2018-07-09 21:42:34 +0800

    开启缓存

--- user-edge-service/src/main/java/com/faxe/user/Application.java
+++ user-edge-service/src/main/java/com/faxe/user/Application.java
@@ -2,12 +2,14 @@ package com.faxe.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author ZhouXiang
  * @create 2018-07-07 21:34
  **/
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
     public static void main(String[] args){
