a417df3e65d868472837b33c75236c6c3349d647
ZhouXiang <1041287344@qq.com>
2018-08-26 12:10:10 +0800

    course-service 添加 dubbo

--- course-dubbo-service/src/main/java/com/faxe/dubbo/course/service/CourseServiceImpl.java
+++ course-dubbo-service/src/main/java/com/faxe/dubbo/course/service/CourseServiceImpl.java
@@ -1,19 +1,22 @@
 package com.faxe.dubbo.course.service;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.faxe.dubbo.course.dao.CourseDao;
 import com.faxe.dubbo.course.entity.Course;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * 服务provider
  *
+ * @service 为 dubbo 注解
+ * application.yml 中  scan: com.faxe.dubbo.course.service 会扫描这个注解
+ *
  * @author ZhouXiang
  * @create 2018-08-25 23:53
  **/
-@Service
+@Service(version = "V1.0.0")
 public class CourseServiceImpl implements CourseService<String> {
     @Autowired
     private CourseDao courseDao;
